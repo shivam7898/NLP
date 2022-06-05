23cb7c0d12573eb08407ee17ba0fb55d41c538dc
shivam7898 <shivam7898@users.noreply.github.com>
2022-06-05 15:35:25 +0530

    add: modify: Stopped Auto Conversion of Python Objects to R to prevent Error

--- code/B01Modules.py
+++ code/B01Modules.py
@@ -1,16 +1,23 @@
 # # reticulate::source_python(here::here("code", "B01Modules.py")) ===============================#
-import os
-import sys
-import platform
-import pip
-import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn
-import random 
-import copy
-import gc
-import igraph as ig
-import cairo
+
+if ('pandas' not in sys.modules):
+    import os
+    import sys
+    import platform
+    import pip
+    import pandas as pd
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import sklearn
+    import random 
+    import copy
+    import gc
+    import igraph as ig
+    import cairo
+    #from types import ModuleType as MT
+else:
+    print("Python Modules not loaded again.")
+
+# #
