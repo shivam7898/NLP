8849127b2f19cb0c51f7865b8f49f7d961cd184d
shivam7898 <shivam7898@users.noreply.github.com>
2022-06-05 10:36:09 +0530

    clean: tab to space, space at line end, align etc.

--- code/A00Knitr.R
+++ code/A00Knitr.R
@@ -1,22 +1,19 @@
 # # source(here::here("code", "A00Knitr.R")) =====================================================#
 
 if (!("q_start" %in% ls(envir = .GlobalEnv))) {
-  # #Compile Start Time 
+  # #Compile Start Time
   q_start <- Sys.time()
-  knitr::opts_chunk$set(echo = TRUE, include = TRUE, eval = TRUE, 
-						collapse = TRUE,
-						width = 100,
-                        opts.label = 'twofig',
-                        message = FALSE, warning = FALSE, error = FALSE, 
-                        progress = FALSE, verbose = FALSE)			
+  knitr::opts_chunk$set(echo = TRUE, include = TRUE, eval = TRUE, collapse = TRUE, width = 100,
+                        opts.label = 'twofig', message = FALSE, warning = FALSE, error = FALSE,
+                        progress = FALSE, verbose = FALSE)
   # #Store the Run Time of slow Chunks
-  q_all_chunk_times <- list()  
+  q_all_chunk_times <- list()
   knitr::knit_hooks$set(chunk_run_time = local({
     now <- NULL
     function(before, options) {
       if (before) {
-        # #record the current time before each chunk using "<<-"
-        now <<- Sys.time()
+          # #record the current time before each chunk using "<<-"
+          now <<- Sys.time()
       } else {
           # #calculate the time difference after a chunk
           res <- difftime(Sys.time(), now)
@@ -30,12 +27,12 @@ if (!("q_start" %in% ls(envir = .GlobalEnv))) {
   knitr::opts_chunk$set(chunk_run_time = TRUE)
 
   # #Chunk Templates, use as: opts.label='twofig'
-  knitr::opts_template$set(twofig = list(
-					fig.show = "hold", fig.align = 'center', out.width = "49%"))
+  knitr::opts_template$set(twofig = 
+                           list(fig.show = 'hold', fig.align = 'center', out.width = '49%'))
 
-  # The Standard, Wider, and Higher Plot Sizes
+  # #The Standard, Wider, and Higher Plot Sizes
   q_png_h <- 7
-  q_png_w  <- 7
+  q_png_w <- 7
   q_png_w_w <- 2 * q_png_w
   q_png_w_h <- {9/16} * q_png_w_w
   q_png_h_h <- {3/4} * 2 * q_png_h
@@ -49,50 +46,51 @@ if (!("q_start" %in% ls(envir = .GlobalEnv))) {
   q_exm = list()
   knitr::knit_hooks$set(engine = function(before, options) {
     # #Set the hook once to handle All /both lists
-    if ( before ) {
-      if ( options$engine == "theorem" ) {
-	         q_thm[[options$label]] <<- options$name
-	     } else if ( options$engine == "definition" ) {
+    if (before) {
+      q_engine <- options$engine
+      if (q_engine == "theorem") {
+          q_thm[[options$label]] <<- options$name
+      } else if (q_engine == "definition") {
           q_def[[options$label]] <<- options$name
-      } else if ( options$engine == "conjecture" ) {
+      } else if (q_engine == "conjecture") {
           q_cnj[[options$label]] <<- options$name
-      } else if ( options$engine == "example" ) {
+      } else if (q_engine == "example") {
           q_exm[[options$label]] <<- options$name
       }
     }
     NULL
   })
-  
-  # #Chunk Colour Change for Python & R
+
+  # #Chunk Decoration based on Language Engine: R & Python
   # #https://f0nzie.github.io/matplotlib-with-rmarkdown/
+  # #NOTE: Global Chunk Option setup leads to problem with chunks like "definition" etc.
   knitr::knit_hooks$set(decorate = function(before, options) {
     if (before) {
-       if (options$engine == "python") {
-         label <- "<b> Python</b>"
-         bc <- "#417FB1"
-         sz <- "100%"
-         tc <- "#FFD94C"
-         icon <- fa("python", fill = tc)
-       } else if (options$engine == "R") {
-         label <- "<b> R</b>"
-         bc <- "#4C78DB"
-         sz <- "100%"
-         tc <- "#ffffff"
-		 icon <- fa("r-project", fill = tc)
-       } #else if (options$engine == "bash") {
-       #  label <- "<b>Shell</b>"
-       #  bc <- "#000000"
-       #  sz <- "100%"
-       #  tc <- "#ffffff"
-       #  icon <- "<i class=\"fas fa-terminal\"></i>"
-       #}
-      paste0("<div class=decocode>",
-             "<div style=\"background-color:", bc, "\">",
-             "<span style=\"font-size:", sz, ";color:", tc, "\">", icon,  
-             label, "</span>")
+      q_engine <- options$engine
+      if (q_engine == "python") {
+          label <- "<b> Python</b>"
+          bc <- "#417FB1"
+          sz <- "100%"
+          tc <- "#FFD94C"
+          icon <- fa("python", fill = tc)
+      } else if (q_engine == "R") {
+          label <- "<b> R</b>"
+          bc <- "#4C78DB"
+          sz <- "100%"
+          tc <- "#ffffff"
+          icon <- fa("r-project", fill = tc)
+      } #else if (options$engine == "bash") {
+      #    label <- "<b>Shell</b>"
+      #    bc <- "#000000"
+      #    sz <- "100%"
+      #    tc <- "#ffffff"
+      #    icon <- "<i class=\"fas fa-terminal\"></i>"
+      #}
+      paste0("<div class=decocode>", "<div style=\"background-color:", bc,
+             "\">", "<span style=\"font-size:", sz, ";color:", tc, "\">", icon, label, "</span>")
     } else {
-      "</div><br></div>"
+        "</div><br></div>"
     }
-  })   
+  })
 } else print("Knitr not loaded again.")
 
